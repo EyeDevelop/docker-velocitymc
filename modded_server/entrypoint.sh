a88aaf8ece6b5c948ca45b834ebd6a81f3507d7f
Hans Goor <me@eyedevelop.org>
2021-02-22 15:50:56 +0100

    Now using optimised flags from Aikar.

--- modded_server/entrypoint.sh
+++ modded_server/entrypoint.sh
@@ -39,4 +39,12 @@ if [[ ! -f "/run.sh" ]]; then
     chmod +x /run.sh
 fi
 
-exec su-exec mcuser:mcuser /run.sh -jar forge.jar
+exec su-exec mcuser:mcuser /run.sh\
+    -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200\
+    -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch\
+    -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M\
+    -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4\
+    -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90\
+    -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem\
+    -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true\
+    -jar forge.jar
